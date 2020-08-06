28eb84d92f58464e383471053ff3168eccff9f2f
bh-oussama97 <oussama.benhassen1@esprit.tn>
2020-08-06 21:44:16 +0100

    anugular components part 1

---
+++ conFusion/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'conFusion';
+}
