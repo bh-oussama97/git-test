5830b6f8ab6a880bf646c9d22bbe6551f70ecda9
bh-oussama97 <oussama.benhassen1@esprit.tn>
2020-08-09 23:44:00 +0100

    single page applications part 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,23 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import {PromotionService} from './services/promotion.service';
+import {AppRoutingModule } from './app-routing/app-routing.module';
 
 
 @NgModule({
@@ -26,22 +28,26 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
     BrowserModule,
-    AppRoutingModule,
     MatListModule,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
-    MatGridListModule
-  
+    MatGridListModule,
+    AppRoutingModule
+
   ],
-  providers: [DishService],
+  providers: [DishService,
+  PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
