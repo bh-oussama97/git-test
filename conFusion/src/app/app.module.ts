10bc58caf7f13ca682bdd05be79fcf3c05b046ac
bh-oussama97 <oussama.benhassen1@esprit.tn>
2020-08-17 12:00:18 +0100

    promise + template validation

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
-import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,6 +11,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { LeaderService} from './services/leader.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
@@ -20,8 +19,18 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import {PromotionService} from './services/promotion.service';
 import {AppRoutingModule } from './app-routing/app-routing.module';
-
-
+import { MatDialogModule } from '@angular/material/dialog';
+import {LoginComponent} from './login/login.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
+import{ MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import 'hammerjs';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +40,9 @@ import {AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
+    
   ],
   imports: [
     BrowserAnimationsModule,
@@ -43,11 +54,23 @@ import {AppRoutingModule } from './app-routing/app-routing.module';
     MatIconModule,
     MatCardModule,
     MatGridListModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
 
   ],
   providers: [DishService,
-  PromotionService],
+  PromotionService,LeaderService],
+  entryComponents :[
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ conFusion/src/app/services/leader.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {Leader } from '../shared/leader';
+import {LEADERS} from '../shared/leaders';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaderService {
+
+  getLeaders(): Promise<Leader[]> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS),2000);
+    });
+  }
+
+  getLeader(id : string) : Promise<Leader> {
+   return new Promise(resolve => {
+setTimeout(()=> resolve (LEADERS.filter(leader => leader.id === id)[0]),2000);
+   });
+  }
+  
+  
+  getFeaturedLeader(): Promise<Leader> {
+    
+    return new Promise(resolve => {
+      setTimeout(()=> resolve (LEADERS.filter((lead)=> lead.featured)[0]),2000);
+         });
+
+  }
+
+  constructor() { }
+}
