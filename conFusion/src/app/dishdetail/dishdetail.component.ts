29d64632bdbc977291acf9ef6c981050a215e1b1
bh-oussama97 <oussama.benhassen1@esprit.tn>
2020-08-26 00:55:53 +0100

    saving changes to the server'

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -27,6 +27,7 @@ rating:number;
    next : string ;
    dishcopy :Dish;
    visibility = 'shown' ;
+
   
 
    @ViewChild('fform1') feedbackFormDirective; 
@@ -78,7 +79,9 @@ rating:number;
 
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+    .subscribe(dish => { this.dish = dish; 
+      this.dishcopy = dish;
+      this.setPrevNext(dish.id); },
     errmess=> this.errMess = <any>errmess
     );
   }
@@ -139,8 +142,16 @@ rating:number;
     });
     
     //this.dish.comments.push(this.feedback1); //push after submit
-    this.dish.comments.push(this.comment);
-
+    this.dishcopy.comments.push(this.comment);
+this.dishService.putDish(this.dishcopy).subscribe(dish => {
+this.dish = dish;
+this.dishcopy = dish;
+},
+errmess => {
+  this.dish = null;
+  this.dishcopy = null;
+  this.errMess = <any>errmess
+});
     this.feedbackFormDirective.resetForm({
       author: '',
       rating: 5,
